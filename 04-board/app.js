1776c403cc463ca343e045b1396868663ffd125f
Nastia <70321720+veroivska@users.noreply.github.com>
2021-07-20 15:02:07 +0300

    init: board

---
+++ 04-board/app.js
@@ -0,0 +1,37 @@
+const board = document.querySelector('#board');
+const SQUARES_NUMBER = 340;
+
+const colors = ['#7C83FD', '#96BAFF', '#7DEDFF', '#88FFF7', '#FFC1F3', '#A275E3'] 
+
+for(let i = 0; i < SQUARES_NUMBER; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+    })
+
+    square.addEventListener('mouseleave', () => {
+        removeColor(square)
+    })
+
+    board.append(square);
+}
+
+function setColor(elem){
+    const color = getRandomColor();
+    elem.style.backgroundColor = color;
+    elem.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(elem){
+    elem.style.backgroundColor = '#1d1d1d'
+    elem.style.boxShadow = '0 0 2px #000'
+
+}
+
+function getRandomColor(){
+   const index = Math.floor(Math.random() * colors.length);
+   return colors[index];
+}
+
